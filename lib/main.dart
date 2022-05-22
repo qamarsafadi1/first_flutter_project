3d58a8eb70784a26c848ece3390854571b58ed67
qamarelsafadi <qamar.elsafadi98@gmail.com>
2022-05-22 15:02:25 +0300

    init quizzer app

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,11 @@ import 'package:english_words/english_words.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(XylophoneApp());
+  runApp(QuizzerApp());
 }
 
-class XylophoneApp extends StatelessWidget {
-  const XylophoneApp({Key? key}) : super(key: key);
-
-  void playSound(int soundNumber) {
-    final player = AudioCache();
-    player.play('assets_note$soundNumber.wav');
-  }
-
-  Expanded buildKeys({required int soundNumber, required MaterialColor color}) {
-    return Expanded(
-      child: TextButton(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(color),
-          ),
-          onPressed: () {
-            playSound(soundNumber);
-          },
-          child: Text('')),
-    );
-  }
+class QuizzerApp extends StatelessWidget {
+  const QuizzerApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -33,18 +15,7 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              buildKeys(soundNumber: 1, color: Colors.red),
-              buildKeys(soundNumber: 2, color: Colors.orange),
-              buildKeys(soundNumber: 3, color: Colors.yellow),
-              buildKeys(soundNumber: 4, color: Colors.green),
-              buildKeys(soundNumber: 5, color: Colors.teal),
-              buildKeys(soundNumber: 6, color: Colors.blue),
-              buildKeys(soundNumber: 7, color: Colors.purple),
-            ],
-          ),
+          child: Container(),
         ),
       ),
     );
