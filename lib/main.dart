760549fbb1f76762e691c2019d1865800c181ba6
qamarelsafadi <qamar.elsafadi98@gmail.com>
2022-05-22 11:44:07 +0300

    finishing up the dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,67 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
       backgroundColor: Colors.red,
-      appBar: AppBar(title: Text('Dicee'), backgroundColor: Colors.red),
-      body: DiceePage(),
+      appBar: AppBar(title: Text('Dice'), backgroundColor: Colors.red),
+      body: DicePage(),
     ),
   ));
 }
 
-class DiceePage extends StatelessWidget {
-  const DiceePage({Key? key}) : super(key: key);
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDice();
+              },
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                  EdgeInsets.all(16),
+                ),
+              ),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDice();
+              },
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                  EdgeInsets.all(16),
+                ),
+              ),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void changeDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
